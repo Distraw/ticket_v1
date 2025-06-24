85a3682e78171ea1d0b1888cbbaff281c627ec8e
Distraw <dmitroisuk11@gmail.com>
2025-06-24 15:46:33 +0300

    01_struct - initial implementation

---
+++ 01_struct/target/debug/deps/struct_-31e5b2a75083b8e1.d
@@ -0,0 +1,5 @@
+/home/shrek/Projects/Rust/ticket_v1/01_struct/target/debug/deps/struct_-31e5b2a75083b8e1: src/lib.rs
+
+/home/shrek/Projects/Rust/ticket_v1/01_struct/target/debug/deps/struct_-31e5b2a75083b8e1.d: src/lib.rs
+
+src/lib.rs:
