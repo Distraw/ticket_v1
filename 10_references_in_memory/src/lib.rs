0ae128b28877fff645bd7aa57fcdf2cc2caa5e55
Distraw <dmitroisuk11@gmail.com>
2025-07-02 09:59:25 +0300

    Introduces constant values to tests instead of dynamic that can fail occasionally

--- 10_references_in_memory/src/lib.rs
+++ 10_references_in_memory/src/lib.rs
@@ -9,18 +9,20 @@ mod tests {
     use super::Ticket;
     use std::mem::size_of;
 
+    const USIZE_64BIT_IN_BYTES: usize = 8;
+
     #[test]
     fn u16_ref_size() {
-        assert_eq!(size_of::<&u16>(), size_of::<usize>());
+        assert_eq!(size_of::<&u16>(), USIZE_64BIT_IN_BYTES);
     }
 
     #[test]
     fn u64_mut_ref_size() {
-        assert_eq!(size_of::<&mut u64>(), size_of::<usize>());
+        assert_eq!(size_of::<&mut u64>(), USIZE_64BIT_IN_BYTES);
     }
 
     #[test]
     fn ticket_ref_size() {
-        assert_eq!(size_of::<&Ticket>(), size_of::<usize>());
+        assert_eq!(size_of::<&Ticket>(), USIZE_64BIT_IN_BYTES);
     }
 }
